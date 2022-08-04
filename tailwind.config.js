f9f0f3d067af9b2244e982cdef3ec56d6f24f0b5
Galihxirpl <87462790+Galihxirpl@users.noreply.github.com>
2022-08-04 14:24:33 +0700

    Galih Satriya Pratama-12 RPL-15

---
+++ tailwind.config.js
@@ -0,0 +1,34 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './public/index.html',
+    './src/**/*.{html,js}',
+  ],
+
+  theme: {
+    extend: {
+      colors: {
+        ungu: "#5267Df",
+        merah: "#FA5959",
+        biru: "#242A45",
+        abu: "#9194A2",
+        putih: "#f7f7f7",
+      },
+
+      fontFamily: {
+        Poppins: ["Poppins, sans-serif"],
+      },
+
+      container: {
+        center: true,
+        padding: "1rem",
+        screens: {
+          lg: "1124px",
+          xl: "1124px",
+          "2xl": "1124px",
+        },
+      },
+    },
+  },
+  plugins: [],
+};
